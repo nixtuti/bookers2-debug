6e55c226087bc1555f2a9ecc834caf1be07acf0c
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-09-24 03:27:04 +0000

    [Update]5-2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :books
+  has_many :books
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -12,6 +12,11 @@ class User < ApplicationRecord
   
   
   def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+    if profile_image.attached?
+      profile_image
+    else
+      'no_image.jpg'
+    end
   end
+  
 end
