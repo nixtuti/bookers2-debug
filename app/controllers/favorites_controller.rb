6a54cb4e416f5122a99d9cda737d2d7cafd1e478
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-09-24 09:56:25 +0000

    [Start]5-3

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,15 @@
+class FavoritesController < ApplicationController
+  
+  def create
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorite.new(book_id: book.id)
+    favorite.save
+  end
+  
+  def destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorite.find_by(book_id: book.id)
+    favorite.destroy
+  end
+  
+end
