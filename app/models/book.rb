813256ca26b90c2ba4fde0f2ca32d45f7adb8149
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-09-27 08:47:52 +0000

    [Update]5-5

--- app/models/book.rb
+++ app/models/book.rb
@@ -12,13 +12,13 @@ class Book < ApplicationRecord
   
   #検索方法分岐
   def self.looks(search, word)
-    if search == perfect_match
+    if search == "perfect_match"
       @book = Book.where("title_LIKE?", "#{word}")
-    elsif search == forward_match
+    elsif search == "forward_match"
       @book = Book.where("title_LIKE?", "#{word}%")
-    elsif search == backward_match
+    elsif search == "backward_match"
       @book = Book.where("title_LIKE?", "%#{word}")
-    elsif search == partial_match
+    elsif search == "partial_match"
       @book = Book.where(["title_LIKE? OR body_LIKE?", "%#{word}%", "%#{word}%"])
     else
       @book = Book.all
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,8 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   
-  get 'search' => 'searchs#search'
+  get 'search' => 'searches#search'
+ 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
