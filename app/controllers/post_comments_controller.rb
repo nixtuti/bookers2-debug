ef8360469d32ad77615f3a1de83b9f265332fe53
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-09-29 10:47:34 +0000

    [Add]5-6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,19 +1,22 @@
 class PostCommentsController < ApplicationController
   
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(post_comment_params)
+    @book = Book.find(params[:book_id])
+    @post_comment = current_user.post_comments.new(post_comment_params)
     #下記の省略形
       #comment = PostComment.new(post_comment_params)
       #comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @post_comment.book_id = @book.id
+    @post_comment.save
+    #redirect_to book_path(book)(非同期通信化)
+    render 'post_comments/post_comments'
   end
   
   def destroy
     PostComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    #redirect_to book_path(params[:book_id])(非同期通信化)
+    @book = Book.find(params[:book_id])
+    render 'post_comments/post_comments'
   end
   
   private
