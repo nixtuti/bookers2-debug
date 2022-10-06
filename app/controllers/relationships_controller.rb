ecd5b243e5eaea9e8d0ce7ba16e7c193cec37ffc
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-10-06 08:08:16 +0000

    [Fix]6-4

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,7 @@
 class RelationshipsController < ApplicationController
   
   def create
-    user = User.find(params[:user_id])
-    current_user.follow(user)
+    current_user.follow(params[:user_id])
     redirect_to request.referer
   end
   
